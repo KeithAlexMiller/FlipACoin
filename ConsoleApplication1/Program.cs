138aa56e416425d0aae164728f7aab4257e42482
Keith Miller <keithalexmiller@gmail.com>
2015-04-07 16:32:20 -0600

    Added function FlipACoin

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static Random rng = new Random();
+        static int headsCounter = 0;
+        static int tailsCounter = 0;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Flipped: " + FlipACoin());
+            }
+            Console.WriteLine("#Heads: " + headsCounter);
+            Console.WriteLine("#Tails: " + tailsCounter);
+            Console.ReadKey();
+        }
+        public static string FlipACoin()
+        {
+            if (rng.Next(2) == 0)
+            {
+                //heads
+                headsCounter++;
+                return "Heads";
+            }
+            else
+            {
+                //tails
+                tailsCounter++;
+                return "Tails";
+            }
+        }
+    }
+}
